70222ac69771c3f02f759db667cd18377c699e2e
Vitor Darela <vitor.darela@farfetch.com>
2023-01-17 16:49:03 +0000

    fix links

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -69,10 +69,10 @@ const Hero = ({
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                  <Button tag="a" color="primary" wideMobile href="#">
                     Get started
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  <Button tag="a" color="dark" wideMobile href="#">
                     View our Projects
                     </Button>
                 </ButtonGroup>
