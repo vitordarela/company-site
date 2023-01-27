813c09222cd4694e68269675069562187a813723
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-27 17:48:07 +0100

    fix(update): views folder and names

---
+++ src/views/CloudServers.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import CloudServersComponent from '../components/sections/CloudServers'
+
+// import sections
+import Cta from '../components/sections/Cta'
+
+const CloudServers = () => {
+  return (
+    <>
+      <CloudServersComponent
+        invertMobile
+        topDivider
+        imageFill
+        className="illustration-section-02"
+      />
+      <Cta split />
+    </>
+  )
+}
+
+export default CloudServers
