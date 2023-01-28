512e0590abe29a2028ab4b23c5b2c02234d140a7
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-29 00:26:01 +0100

    feat: add hook-form to register and ref

--- src/components/elements/Input.js
+++ src/components/elements/Input.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import FormLabel from './FormLabel';
-import FormHint from './FormHint';
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import FormLabel from './FormLabel'
+import FormHint from './FormHint'
 
 const propTypes = {
   children: PropTypes.node,
   label: PropTypes.string,
   labelHidden: PropTypes.bool,
-  type: PropTypes.oneOf(['textarea', 'text', 'email', 'tel', 'password', 'number', 'search', 'color', 'date', 'time', 'datetime-local']),
+  type: PropTypes.oneOf([
+    'textarea',
+    'text',
+    'email',
+    'tel',
+    'password',
+    'number',
+    'search',
+    'color',
+    'date',
+    'time',
+    'datetime-local',
+  ]),
   name: PropTypes.string,
   status: PropTypes.string,
   disabled: PropTypes.bool,
@@ -18,7 +30,7 @@ const propTypes = {
   size: PropTypes.string,
   placeholder: PropTypes.string,
   rows: PropTypes.number,
-  hint: PropTypes.string
+  hint: PropTypes.string,
 }
 
 const defaultProps = {
@@ -35,10 +47,11 @@ const defaultProps = {
   size: '',
   placeholder: '',
   rows: 3,
-  hint: null
+  hint: null,
 }
 
 const Input = ({
+  register = null,
   className,
   children,
   label,
@@ -56,31 +69,35 @@ const Input = ({
   hint,
   ...props
 }) => {
-
   const wrapperClasses = classNames(
-    (formGroup && formGroup !== '') && (formGroup === 'desktop' ? 'form-group-desktop' : 'form-group'),
-    (hasIcon && hasIcon !== '') && 'has-icon-' + hasIcon
-  );
+    formGroup &&
+      formGroup !== '' &&
+      (formGroup === 'desktop' ? 'form-group-desktop' : 'form-group'),
+    hasIcon && hasIcon !== '' && 'has-icon-' + hasIcon,
+  )
 
   const classes = classNames(
     'form-input',
     size && `form-input-${size}`,
     status && `form-${status}`,
-    className
-  );
+    className,
+  )
 
-  const Component = type === 'textarea' ? 'textarea' : 'input';
+  const Component = type === 'textarea' ? 'textarea' : 'input'
   return (
     <>
-      {label && <FormLabel labelHidden={labelHidden} id={props.id}>{label}</FormLabel>}
-      <div
-        className={wrapperClasses}
-      >
+      {label && (
+        <FormLabel labelHidden={labelHidden} id={props.id}>
+          {label}
+        </FormLabel>
+      )}
+      <div className={wrapperClasses}>
         <Component
           {...props}
           type={type !== 'textarea' ? type : null}
           className={classes}
-          name={name}
+          name={!register ? name : null}
+          {...(register ? register(name) : null)}
           disabled={disabled}
           value={value}
           placeholder={placeholder}
@@ -90,10 +107,10 @@ const Input = ({
       </div>
       {hint && <FormHint status={status}>{hint}</FormHint>}
     </>
-  );
+  )
 }
 
-Input.propTypes = propTypes;
-Input.defaultProps = defaultProps;
+Input.propTypes = propTypes
+Input.defaultProps = defaultProps
 
-export default Input;
+export default Input
