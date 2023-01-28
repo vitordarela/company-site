a080492b20d78a50342a7292518e2f0bf7b381d5
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-29 00:26:36 +0100

    feat: add button link to contact form

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,15 +1,15 @@
-import React, { useState, useRef, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { Link } from 'react-router-dom';
-import Logo from './partials/Logo';
+import React, { useState, useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { Link } from 'react-router-dom'
+import Logo from './partials/Logo'
 
 const propTypes = {
   navPosition: PropTypes.string,
   hideNav: PropTypes.bool,
   hideSignin: PropTypes.bool,
   bottomOuterDivider: PropTypes.bool,
-  bottomDivider: PropTypes.bool
+  bottomDivider: PropTypes.bool,
 }
 
 const defaultProps = {
@@ -17,7 +17,7 @@ const defaultProps = {
   hideNav: false,
   hideSignin: false,
   bottomOuterDivider: false,
-  bottomDivider: false
+  bottomDivider: false,
 }
 
 const Header = ({
@@ -29,64 +29,66 @@ const Header = ({
   bottomDivider,
   ...props
 }) => {
+  const [isActive, setIsactive] = useState(false)
 
-  const [isActive, setIsactive] = useState(false);
-
-  const nav = useRef(null);
-  const hamburger = useRef(null);
+  const nav = useRef(null)
+  const hamburger = useRef(null)
 
   useEffect(() => {
-    isActive && openMenu();
-    document.addEventListener('keydown', keyPress);
-    document.addEventListener('click', clickOutside);
+    isActive && openMenu()
+    document.addEventListener('keydown', keyPress)
+    document.addEventListener('click', clickOutside)
     return () => {
-      document.removeEventListener('keydown', keyPress);
-      document.removeEventListener('click', clickOutside);
-      closeMenu();
-    };
-  });  
+      document.removeEventListener('keydown', keyPress)
+      document.removeEventListener('click', clickOutside)
+      closeMenu()
+    }
+  })
 
   const openMenu = () => {
-    document.body.classList.add('off-nav-is-active');
-    nav.current.style.maxHeight = nav.current.scrollHeight + 'px';
-    setIsactive(true);
+    document.body.classList.add('off-nav-is-active')
+    nav.current.style.maxHeight = nav.current.scrollHeight + 'px'
+    setIsactive(true)
   }
 
   const closeMenu = () => {
-    document.body.classList.remove('off-nav-is-active');
-    nav.current && (nav.current.style.maxHeight = null);
-    setIsactive(false);
+    document.body.classList.remove('off-nav-is-active')
+    nav.current && (nav.current.style.maxHeight = null)
+    setIsactive(false)
   }
 
   const keyPress = (e) => {
-    isActive && e.keyCode === 27 && closeMenu();
+    isActive && e.keyCode === 27 && closeMenu()
   }
 
   const clickOutside = (e) => {
     if (!nav.current) return
-    if (!isActive || nav.current.contains(e.target) || e.target === hamburger.current) return;
-    closeMenu();
-  }  
+    if (
+      !isActive ||
+      nav.current.contains(e.target) ||
+      e.target === hamburger.current
+    )
+      return
+    closeMenu()
+  }
 
   const classes = classNames(
     'site-header',
     bottomOuterDivider && 'has-bottom-divider',
-    className
-  );
+    className,
+  )
 
   return (
-    <header
-      {...props}
-      className={classes}
-    >
+    <header {...props} className={classes}>
       <div className="container">
-        <div className={
-          classNames(
+        <div
+          className={classNames(
             'site-header-inner',
-            bottomDivider && 'has-bottom-divider'
-          )}>
+            bottomDivider && 'has-bottom-divider',
+          )}
+        >
           <Logo />
-          {!hideNav &&
+          {!hideNav && (
             <>
               <button
                 ref={hamburger}
@@ -100,42 +102,50 @@ const Header = ({
               </button>
               <nav
                 ref={nav}
-                className={
-                  classNames(
-                    'header-nav',
-                    isActive && 'is-active'
-                  )}>
+                className={classNames('header-nav', isActive && 'is-active')}
+              >
                 <div className="header-nav-inner">
-                  <ul className={
-                    classNames(
+                  <ul
+                    className={classNames(
                       'list-reset text-xs',
-                      navPosition && `header-nav-${navPosition}`
-                    )}>
+                      navPosition && `header-nav-${navPosition}`,
+                    )}
+                  >
                     <li>
-                    <Link to="/technologies" onClick={closeMenu}>Technologies</Link>
+                      <Link to="/technologies" onClick={closeMenu}>
+                        Technologies
+                      </Link>
                     </li>
                     <li>
-                    <Link to="/cloud/servers" onClick={closeMenu}>Cloud Servers</Link>
+                      <Link to="/cloud/servers" onClick={closeMenu}>
+                        Cloud Servers
+                      </Link>
                     </li>
                   </ul>
-                  {!hideSignin &&
-                    <ul
-                      className="list-reset header-nav-right"
-                    >
+                  {!hideSignin && (
+                    <ul className="list-reset header-nav-right">
                       <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Talk with us</Link>
+                        <Link
+                          to="/contact/formular"
+                          className="button button-primary button-wide-mobile button-sm"
+                          onClick={closeMenu}
+                        >
+                          Talk with us
+                        </Link>
                       </li>
-                    </ul>}
+                    </ul>
+                  )}
                 </div>
               </nav>
-            </>}
+            </>
+          )}
         </div>
       </div>
     </header>
-  );
+  )
 }
 
-Header.propTypes = propTypes;
-Header.defaultProps = defaultProps;
+Header.propTypes = propTypes
+Header.defaultProps = defaultProps
 
-export default Header;
+export default Header
