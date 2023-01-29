4eabe5a724342fd731eebb97240ebb72888f1a3e
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-01-29 14:20:41 +0100

    feat: add modal, form success. fix: text variables

--- src/components/sections/contact/ContactForm.js
+++ src/components/sections/contact/ContactForm.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames'
 import { SectionSplitProps } from '../../../utils/SectionProps'
 import SectionHeader from '../partials/SectionHeader'
 import FormElements from './formular/formElements'
+import Modal from '../../elements/Modal'
+import FormSuccess from './formular/formSuccess'
 
 const propTypes = {
   ...SectionSplitProps.types,
@@ -41,28 +43,43 @@ const ContactFormComponent = ({
     bottomDivider && 'has-bottom-divider',
   )
 
+  const [formModalActive, setFormModalActive] = useState(false)
+
+  const openModal = () => {
+    setFormModalActive(true)
+  }
+
+  const closeModal = () => {
+    setFormModalActive(false)
+  }
+
   function handleSubmitDataToEmail(data) {
     console.log('name', data.name)
     console.log('company', data.company)
     console.log('email', data.email)
     console.log('phone', data.phone)
     console.log('description', data.description)
+
+    if (data) {
+      openModal()
+    }
   }
 
   const sectionHeader = {
     title: 'Request a quote',
     paragraph:
       'We are always open to collaborations where we can all be a part of the same team. Ready to move forward?',
-    sections: [
-      {
-        title: 'Tell us about yourself',
-        paragraph: null,
-      },
-      {
-        title: 'Tell us about your project',
-        paragraph: null,
-      },
-    ],
+  }
+
+  const formContent = {
+    titles: ['Tell us about yourself', 'Tell us about your project'],
+    paragraph: null,
+  }
+
+  const modalHeader = {
+    title: 'Request sent',
+    paragraph:
+      'Your request was successfully sent! We will respond as quickly as possible.',
   }
 
   return (
@@ -71,9 +88,16 @@ const ContactFormComponent = ({
         <div className={innerClasses}>
           <SectionHeader data={sectionHeader} className="center-content" />
           <FormElements
-            data={sectionHeader}
+            data={formContent}
             handleSubmitData={handleSubmitDataToEmail}
           />
+          <Modal
+            id="form-modal"
+            show={formModalActive}
+            handleClose={closeModal}
+          >
+            <FormSuccess data={modalHeader} handleClose={closeModal} />
+          </Modal>
         </div>
       </div>
     </section>
