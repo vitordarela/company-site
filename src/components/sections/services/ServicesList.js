6d09a87cdaded0af2d7f00ca8a8294daa2bb4d97
Vitor Darela <vitor.darela@farfetch.com>
2023-02-28 09:59:48 +0000

    test render metadata

--- src/components/sections/services/ServicesList.js
+++ src/components/sections/services/ServicesList.js
@@ -43,25 +43,9 @@ const ServicesList = ({
 
   const tilesClasses = classNames('tiles-wrap', pushLeft && 'push-left')
 
-  const sectionHeader = serviceData;
-
   return (
     <section {...props} className={outerClasses}>
-       <Head>
-        <meta name="description" content={ "CodeBe "+ sectionHeader.title + " - " + sectionHeader.paragraph } key="description"/>
-        <meta name="title" content={ "CodeBe - " + sectionHeader.title +" - " + sectionHeader.paragraph } key="title"/>
-
-        <meta property="og:type" content="website" key="og:type"/>
-        <meta property="og:title" content={ "CodeBe - " + sectionHeader.title +" - " + sectionHeader.paragraph } key="og:title"/>
-        <meta property="og:description" content={ "CodeBe "+ sectionHeader.title + " - " + sectionHeader.paragraph } key="og:description"/>
-        <meta property="og:url" content={ "https://www.codebe.dev/services" } key="og:url"/>
-
-        <meta property="twitter:title" content={ "CodeBe "+ sectionHeader.title + " - " + sectionHeader.paragraph } key="twitter:title"/>
-        <meta property="twitter:description" content={ "CodeBe "+ sectionHeader.title + " - " + sectionHeader.paragraph } key="twitter:description"/>
-        <meta property="twitter:url" content={ "https://www.codebe.dev/services" } key="twitter:url"/>
-
-        <title>CodeBe - { sectionHeader.title +" - " + sectionHeader.paragraph }</title>
-      </Head>
+       
       <div className="container">
         <div className={innerClasses}>
           <SectionHeader data={sectionHeader} className="center-content" />
