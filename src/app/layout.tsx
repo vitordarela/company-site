643403bb76a28cccacd15aee39e1b60fae6fb56a
Vitor Darela <vitor.darela@farfetch.com>
2023-03-12 00:53:39 +0000

    try fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,13 +19,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
 
         <Script strategy="lazyOnload">
           {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GA_CODE}', {
-        page_path: window.location.pathname,
-        });
-    `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_CODE}', {
+            page_path: window.location.pathname,
+            });
+          `}
         </Script>
       </head>
       <body>
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,5 @@
 export {};
 
-type ReactNode = ReactChild | ReactFragment | ReactPortal | boolean | null | undefined;
-
 declare global {
   interface Window {
     gtag: any; // 👈️ turn off type checking
